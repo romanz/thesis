0f0bdb247225cc9cbd0eea0605e7084e9f9623e9
Roman Zeyde <roman.zeyde@gmail.com>
2010-02-15 22:48:46 +0200

    refactor Poisson solver

--- poisson/test.m
+++ poisson/test.m
@@ -1,19 +1,20 @@
-function poisson_test
+function test
 %% Simulate and solve Poisson Equation on a 2D grid using Relaxation
-% The equation to solve is: ?u = f
-% ? is discretized on a grid, and Jacobi iteration is used for solution.
+% The equation to solve is: Laplacian(u) = f.
+% Laplacian is discretized on a grid, and Jacobi iteration is used for solution.
+
 clc
-sz = [3, 1]; % We use NDGRID convention (X is 1st, Y is 2nd)
-[X, Y] = create_grid( linspace(0, 1, sz(1)), linspace(0, 1, sz(2)) );
+sz = [17 9]; % We use NDGRID convention (X is 1st, Y is 2nd)
+[X, Y] = create_grid( linspace(-2, 2, sz(1)), linspace(-1, 1, sz(2)) );
 [U, L] = create_sol(); % The ideal solution and its Laplacian.
 
-% A is discrete version of ?, so we actually solve the system: Av = f
+% We actually solve the linear system: Av = f
 [A, I] = create_laplacian(X, Y, sz);
 V0 = U(X, Y); % The ideal solution (for boundary condition)
 V0(I) = 0; % "Fill" the interior with initial guess
 F = L(X, Y); % right hand side of the equation
-V = jacobi(A, V0, F, I, 1);
-
+V = jacobi(A, V0, F, I, 100)
+mesh(X, Y, V); xlabel('X'); ylabel('Y'); 
 save poisson_test
 
 function [v] = jacobi(A, v, f, I, T)
@@ -22,15 +23,28 @@ function [v] = jacobi(A, v, f, I, T)
 % Dv = f-Tv = (f - Av) + Dv
 % v' = v + D^{-1} (f - Av)
 d = diag(A);
-full(d)
-v
+r = zeros(size(v));
+dv = zeros(size(v));
 for t = 1:T
-    Av = A*v; % Apply ? on v.
-    r = f - Av; % Compute residual.
-    dv = r(I) ./ d(I); % Compute an update.
-    v(I) = v(I) + dv; % Update v's interior.
+    Av = A*v(:); % Apply A on v.
+    r(I) = f(I) - Av(I); % Compute residual.
+    dv(I) = r(I) ./ d(I); % Compute an update.
+    v(I) = v(I) + dv(I); % Update v's interior.
 end
-v
+
+function [U, L] = create_sol()
+%% Create solutions for the specific diff. eq. instance.
+% - F is the function itself (for boundary conditions).
+% - L is the Laplacian of F.
+% It may be useful for solver's verification.
+U = @(X, Y) 0*X+10;
+L = @(X, Y) 0*X;
+
+function [X, Y] = create_grid(x, y)
+%% Create a grid specified by {X(k), Y(k)}.
+% X, Y, I are 2D arrays, created by NDGRID, 
+% so X is the 1st coordinate and Y is the 2nd!
+[X, Y] = ndgrid(x, y);
 
 function [A, I] = create_laplacian(X, Y, sz)
 %% Laplacian discretization using sparse matrix
@@ -39,33 +53,20 @@ A = sparse(N, N);
 K = true(sz);
 if sz(1) > 1, K(1, :) = false; K(end, :) = false; end
 if sz(2) > 1, K(:, 1) = false; K(:, end) = false; end
-K = find(K); % Fill only interior points' corresponding rows
+K = find(K); % Fill only interior points
+stencil = [-1  0 0 0 1; ...
+            0 -1 0 1 0];
+stencil_size = size(stencil, 2);
 for k = K(:).'
-    [kx, ky] = ind2sub(sz, k);
-%     kx = kx + [-1 0 0 0 1];
-%     ky = ky + [0 -1 0 1 0];
-%     A(k, sub2ind(sz, kx, ky)) = ...
-%               [1 1 -4 1 1];
-    kx = kx + [-1 0 1];
-    ky = ky + [0 0 0];    
-    A(k, sub2ind(sz, kx, ky)) = ...
-              [1 -2 1];
+    [i, j] = ind2sub(sz, k);
+    i = i + stencil(1, :).';
+    j = j + stencil(2, :).';
+    m = sub2ind(sz, i, j);
+    x = X(m);
+    y = Y(m);    
+    P = [ones(stencil_size, 1), x, y, x.*y, x.^2, y.^2] \ eye(stencil_size);
+    P = sum(P(end-1:end, :));
+    A(k, m) = P;
 end
 I = any(A, 2); % Find out interior points
-
-function [X, Y] = create_grid(x, y)
-%% PDE utility
-% Create a grid specified by {X(k), Y(k)}.
-% X, Y, I are 2D arrays, created by NDGRID, 
-% so X is the 1st coordinate and Y is the 2nd!
-[X, Y] = ndgrid(x, y);
-
-function [U, L] = create_sol()
-%% Create solutions for the specific diff. eq. instance.
-% - F is the function itself (for boundary conditions).
-% - L is the Laplacian of F.
-% It may be useful for solver's verification.
-a = 2;
-U = @(X, Y) a*X+1;
-L = @(X, Y) 0*X;
 
