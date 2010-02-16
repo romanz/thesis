a2d243d30bd2e5d87f456fbc67f4dfa254d54134
Roman Zeyde <roman.zeyde@gmail.com>
2010-02-16 18:12:39 +0200

    refactor poisson solver

--- poisson/test.m
+++ poisson/test.m
@@ -1,7 +1,6 @@
 %% Simulate and solve Poisson Equation on a 2D grid using Relaxation
 % The equation to solve is: Laplacian(u) = f.
 % Laplacian is discretized on a grid, and Jacobi iteration is used for solution.
-
 clc
 
 % Create solutions for the specific diff. eq. instance.
@@ -16,19 +15,17 @@ sz = 1+[2^5, 0]; % We use NDGRID convention (X is 1st, Y is 2nd)
 [X, Y] = ndgrid( logspace(0, 1, sz(1)), linspace(-1, 1, sz(2)) );
 
 % We actually solve the linear system: Av = f
-[A, I] = create_laplacian(X, Y, sz);
+[A, I] = laplacian(X, Y, sz);
 V0 = U(X, Y); % The ideal solution (for boundary condition)
 Vi = V0; % Initial guess.
 Vi(I) = 0; % "Fill" the interior with initial guess
 F = L(X, Y); % right hand side of the equation
 Vf = jacobi(A, Vi, F, 10000);
-% subplot 131; mesh(X, Y, V0); xlabel('X'); ylabel('Y'); 
-% subplot 132; mesh(X, Y, Vi); xlabel('X'); ylabel('Y'); 
-% subplot 133; mesh(X, Y, Vf-V0); xlabel('X'); ylabel('Y'); 
+
+% Show results
 subplot 121; 
 plot(X, [V0 Vf Vi]); 
 xlabel('X'); title('Solution'); legend('V_0', 'V_f', 'V_i');
 subplot 122; 
 semilogy(X, abs(Vf-V0)); 
 xlabel('X'); title('Error')
-save poisson_test
