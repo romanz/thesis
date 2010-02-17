78434fb8c82aa0d25cd8e8016a1536c4be69b81b
Roman Zeyde <roman.zeyde@gmail.com>
2010-02-17 11:46:54 +0200

    move to 2D

--- poisson/test.m
+++ poisson/test.m
@@ -4,15 +4,15 @@
 clc
 
 % Create solutions for the specific diff. eq. instance.
-% - F is the function itself (for boundary conditions).
+% - U is the function itself (for boundary conditions).
 % - L is the Laplacian of F.
-% It may be useful for solver's verification.
-U = @(X, Y) (10-X).^2;
-L = @(X, Y) 0*X + 2;
+% It is useful for solver's verification.
+U = @(X, Y) X.^3 / 3 + Y.^3 / 2 + X.^2 - Y.^2;
+L = @(X, Y) 2*X + 3*Y;
 
 % Create grid for solver
-sz = 1+[2^5, 0]; % We use NDGRID convention (X is 1st, Y is 2nd)
-[X, Y] = ndgrid( logspace(0, 1, sz(1)), linspace(-1, 1, sz(2)) );
+sz = 1+[2^5, 2^5]; % We use NDGRID convention (X is 1st, Y is 2nd)
+[X, Y] = ndgrid( linspace(-1, 1, sz(1)), linspace(-1, 1, sz(2)) );
 
 % We actually solve the linear system: Av = f
 [A, I] = laplacian(X, Y, sz);
@@ -24,8 +24,9 @@ Vf = jacobi(A, Vi, F, 10000);
 
 % Show results
 subplot 121; 
-plot(X, [V0 Vf Vi]); 
-xlabel('X'); title('Solution'); legend('V_0', 'V_f', 'V_i');
+mesh(X, Y, Vf); 
+xlabel('X'); ylabel('Y'); title('Solution'); 
+
 subplot 122; 
-semilogy(X, abs(Vf-V0)); 
-xlabel('X'); title('Error')
+mesh(X, Y, abs(Vf-V0)); 
+xlabel('X'); ylabel('Y'); title('Error')
