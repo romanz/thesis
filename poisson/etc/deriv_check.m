d4f55a34d15816c86eed1e4cf18be78cd260e63d
Roman Zeyde <roman.zeyde@gmail.com>
2010-02-21 13:46:20 +0200

    move deriv. check script to etc/

--- poisson/etc/deriv_check.m
+++ poisson/etc/deriv_check.m
@@ -1,7 +1,7 @@
 syms x y
-u = exp(x) + sin(y);
-c = cos(x.*y);
+u = sin(x) + cos(y)
+c = exp(x + y)
 g = [diff(u, x); diff(u, y)];
 L = [diff(g(1) * c, x), diff(g(2) * c, y)];
-L = simple(L);
-pretty(L)
+L = simple(sum(L));
+L
