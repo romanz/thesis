4a1a45eb8c9cae858951e292b4e9870b01f00f9a
Roman Zeyde <roman.zeyde@gmail.com>
2010-02-21 16:18:05 +0200

    R -> C

--- poisson/jacobi.m
+++ poisson/jacobi.m
@@ -3,7 +3,7 @@ function [v] = jacobi(A, v, f, interior, T)
 % Av = (D+T)v = f
 % Dv = f-Tv = (f - Av) + Dv
 % J := D^{-1}
-% v' = v + J (f - Av) = (I - JA)v + Jf = Rv + d
+% v' = v + J (f - Av) = (I - JA)v + Jf = Cv + d
 D = diag(A);
 f = f(interior); % Restrict RHS.
 A = A(interior, :); % Restric operator.
@@ -11,15 +11,14 @@ A = A(interior, :); % Restric operator.
 k = nnz(interior);
 J = sparse(1:k, 1:k, 1 ./ D(interior)); % Restrict the inverse.
 
-R = speye(size(A, 2));
-R = R(interior, :) - J * A;
-d = J * f;
+C = speye(size(A, 2));
+C = C(interior, :) - J * A;
+d = J * f(:);
 
 % Keep the original size and convert to column vectors:
 sz = size(v); 
 v = v(:);
-f = f(:);
 for t = 1:T
-    v(interior) = R * v + d; % Update v's interior.
+    v(interior) = C * v + d; % Update v's interior.
 end
 v = reshape(v, sz);
