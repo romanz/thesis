1c37360e74bc7ad7f320eef8dffc56e03c7e9c1e
Roman Zeyde <roman.zeyde@gmail.com>
2010-02-23 14:05:59 +0200

    Add boundary update step to Jacobi iteration.

--- poisson/jacobi.m
+++ poisson/jacobi.m
@@ -1,4 +1,4 @@
-function [v] = jacobi(A, v, f, interior, T, type)
+function [v, C, d] = jacobi(A, v, f, interior, T, type, boundary)
 %% Jacobi iteration:
 % Av = (D+T)v = f
 % Dv = f-Tv = (f - Av) + Dv
@@ -13,16 +13,31 @@ A = A(interior, :); % Restric operator.
 k = nnz(interior);
 J = sparse(1:k, 1:k, 1 ./ D(interior)); % Restrict the inverse.
 I = speye(N);
-% v' = Cv + d
-% C := I - JA
-% d := Jf
-C = sparse(N, N);
-d = zeros(N, 1);
-C(interior, :) = I(interior, :) - J * A;
-d(interior) = J * f(:);
-d(~interior) = v(~interior); % Dirichlet boundary conditions
 
-% Keep the original size and convert to column vectors:
+% Interior update:
+%   v' = Cv + d
+%   C := I - JA (on the interior)
+%   d := Jf
+C_i = speye(N); % Keep boundary values unchanged.
+d_i = zeros(N, 1);
+C_i(interior, :) = I(interior, :) - J * A;
+d_i(interior) = J * f(:);
+
+% Boundary update:
+%   v' = Cv + d
+%   C := 0 (on the boundary)
+%   d := (Dirichlet boundary conditions)
+C_b = speye(N); % Keep interior values unchanged.
+C_b(~interior, :) = 0;
+d_b = zeros(N, 1);
+d_b(~interior) = v(~interior);
+
+% Composition of a boundary update, followed by an interior update
+% v' = Ci(Cb v + db) + di = C_i C_b v + C_i d_b + d_i
+C = C_i * C_b;
+d =  C_i * d_b + d_i;
+
+% Keep the original size and convert to column vector:
 sz = size(v); 
 v = v(:);
 
