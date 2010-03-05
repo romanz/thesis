740f03fcd30e853423b016625df1ac95bf2a2356
Roman Zeyde <roman.zeyde@gmail.com>
2010-03-05 18:08:05 +0200

    refactor Dirichlet conditions

--- poisson/boundary_dirichlet.m
+++ poisson/boundary_dirichlet.m
@@ -1,11 +1,11 @@
-function [A, F] = boundary_dirichlet(A, F, J, V)
+function [A, F] = boundary_dirichlet(A, F, J, X, Y, U)
 if islogical(J)
     J = find(J);
 end
 for k = 1:numel(J)
     j = J(k); % variable #j (to be eliminated)
-    v = V(k); % its value (as a boundary point)
+    u = U(X(j), Y(j)); % its value (as a boundary point)
     I = find(A(:, j)); % corresponding equations
-    F(I) = F(I) - A(I, j) * v; % eliminate and update RHS
+    F(I) = F(I) - A(I, j) * u; % eliminate and update RHS
     A(I, j) = 0; % Update A (this variable is unused)
 end
--- poisson/main.m
+++ poisson/main.m
@@ -4,36 +4,36 @@ function main
 % Laplacian is discretized on a grid, and Jacobi iteration is used.
 
 % Create grid for the solver.
-m = 4;
-x = logspace( -1, 0, 1+2^m); 
-y = logspace( -1, 0, 1+2^m*2); 
+m = 6;
+x = logspace(-1, 0, 1+2^m); 
+y = logspace( -1, 0, 1+2^m); 
 
 % # of iterations
-iters = 8e3;
-type = 'RedBlack';
-% type = 'Jacobi';
+iters = 25e3;
+% type = 'RedBlack';
+type = 'Jacobi';
 
 % We use NDGRID convention (X is 1st, Y is 2nd)
 [X, Y] = ndgrid(x, y);
 sz = [numel(x) numel(y)];
-
 % Create solutions for the specific diff. eq. instance.
 % - U is the function itself (for boundary conditions).
 % - L is the Laplacian of F.
 % It is useful for solver's verification.
-U = @(X, Y) zeros(sz)  +   sin(X) - cos(Y);
-C = @(X, Y) zeros(sz)  +   exp(X - Y);
-L = @(X, Y) zeros(sz)  +   -exp(X - Y) .* (sin(X) - cos(X) - cos(Y) + sin(Y));
+U = @(X, Y) sin(X) - cos(Y);
+C = @(X, Y) exp(X - Y);
+L = @(X, Y) -exp(X - Y) .* (sin(X) - cos(X) - cos(Y) + sin(Y));
 
 % We actually solve the linear system: Av = f
 fprintf('Compute Laplacian operator... '); tic;
 [A, F, I] = laplacian(sz, X, Y, C(X, Y), L(X, Y));
-V0 = U(X, Y); % The ideal solution (for boundary condition)
-[A, F] = boundary(A, F, ~I, V0(~I));
+[A, F] = boundary_dirichlet(A, F, ~I, X, Y, U);
+
 A = A(I, I); F = F(I);
 fprintf('(%.3fs)\n', toc);
 
-Vi = V0(I) * 0; % Initial guess.
+V0 = U(X, Y); % The ideal continuous solution.
+Vi = zeros(nnz(I), 1); % Initial guess zeros(size(V0(I))
 
 fprintf('Construct iterative %s solver... ', type); tic;
 [C, d] = jacobi(A, Vi, F); fprintf('(%.3fs)\n', toc);
