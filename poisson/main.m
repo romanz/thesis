d38db930aadcf1f2d9ab8e0c10c7175301b9c10f
Roman Zeyde <roman.zeyde@gmail.com>
2010-03-07 20:09:00 +0200

    Add an example usage

--- poisson/main.m
+++ poisson/main.m
@@ -1,12 +1,13 @@
 function main
+fprintf('\n');
 %% Simulate and solve Poisson Equation on a 2D grid using Relaxation
 % The equation to solve is: Laplacian(u) = f.
 % Laplacian is discretized on a grid, and Jacobi iteration is used.
 
 % Create grid for the solver.
-m = 6;
-x = logspace(-1, 0, 1+2^m); 
-y = 0; % logspace( -1, 0, 1+2^m); 
+m = 2;
+x = linspace(-1, 1, 1+2^m); 
+y = linspace(-1, 1, 1+2^m); 
 
 % # of iterations
 iters = 25e3;
@@ -33,18 +34,25 @@ if sz(1) > 1
     Bl = ~I & circshift(I, [-1 0]);
     Br = ~I & circshift(I, [+1 0]);
     [A, F] = boundary_dirichlet(A, F, Bl, X, Y, U);
-    [A, F] = boundary_dirichlet(A, F, Br, X, Y, U);
+    [A, F] = boundary_neumann(A, F, Br, [+1 0], X, Y, Ux, Uy);
+%     [A, F] = boundary_dirichlet(A, F, Br, X, Y, U);
+%     [A, F] = boundary_neumann(A, F, Bl, [-1 0], X, Y, Ux, Uy);
 end
 if sz(2) > 1
-    Bu = ~I & circshift(I, [0 -1]);
-    Bd = ~I & circshift(I, [0 +1]); 
+    Bd = ~I & circshift(I, [0 -1]); 
+    Bu = ~I & circshift(I, [0 +1]);
     [A, F] = boundary_dirichlet(A, F, Bu, X, Y, U);
+%     [A, F] = boundary_neumann(A, F, Bd, [0 -1], X, Y, Ux, Uy);
     [A, F] = boundary_dirichlet(A, F, Bd, X, Y, U);
+%     [A, F] = boundary_neumann(A, F, Bu, [0 +1], X, Y, Ux, Uy);
 end
-% [A, F] = boundary_neumann(A, F, ~I, Ux);
+B = A; 
+B(I, I) = 0; 
+assert(nnz(B) == 0); % Verify that boundary variables are eliminated properly.
 
 A = A(I, I); F = F(I);
 fprintf('(%.3fs)\n', toc);
+assert(nnz(isnan(A) | isinf(A)) == 0)
 assert(nnz(A - A') == 0)
 
 Vi = zeros(nnz(I), 1); % Initial guess.
@@ -61,7 +69,13 @@ mat_file = 'results.mat';
 save(mat_file)
 err = show(mat_file); 
 fprintf('error: %e\n', err);
-fprintf('max. eigenvalue: %f\n', max_eigs(C, 1));
+lambda = abs(max_eigs(C, 1));
+fprintf('# iterations ~ %.0f\n', log(eps)/log(lambda));
+full(A), F
 
 function e = max_eigs(A, k)
-e = eigs(A, k, 'lm', struct('disp', 0)); 
+if nnz(A) > 0
+    e = eigs(A, k, 'lm', struct('disp', 0)); 
+else
+    e = 0;
+end
