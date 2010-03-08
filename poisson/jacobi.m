d7a2249efe6e9a134122003de14f90c71dd18f10
Roman Zeyde <roman.zeyde@gmail.com>
2010-03-08 18:36:56 +0200

    Remove unneeded parameter

--- poisson/jacobi.m
+++ poisson/jacobi.m
@@ -1,11 +1,11 @@
-function [R, T, d] = jacobi(A, v, f) %#ok<INUSD>
+function [R, T, d] = jacobi(A, f) %#ok<INUSD>
 %% Compute the appropriate matrices for for Jacobi iteration.
 % Av = [D + (A-D)]v = f
 % Dv = f - (A-D)v = (f - Av) + Dv
 % R := D^{-1}
 % v' = v + R (f - Av) = (I - R A)v + R f
 % v' = Tv + d
-N = numel(v);
+N = numel(f);
 D = diag(A);
 R = sparse(1:N, 1:N, 1./D);
 T = speye(N) - R * A;
