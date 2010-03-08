aeb06d10f2005892f29a9e6b6a2958760f6e144f
Roman Zeyde <roman.zeyde@gmail.com>
2010-03-08 18:36:45 +0200

    Refactor Neumann code

--- poisson/boundary_neumann.m
+++ poisson/boundary_neumann.m
@@ -1,16 +1,22 @@
-function [A, F] = boundary_neumann(A, F, J, delta, X, Y, Ux, Uy)
+function [A, F] = boundary_neumann(A, F, J, P, X, Y, Ux, Uy)
+% Substitute Neumann boundary conditions into Av = f linear system, 
+% where (Ux, Uy) is the gradient, J is one of the grid's boundaries and 
+% P is an unit vector, normal to the boundary, pointing outside of the grid.
 sz = size(J);
 J = find(J);
 for k = 1:numel(J)
     % Jb - boundary variable (to be eliminated)
-    % Ji - interior variable, such that: U(Jb) - U(Ji) = dU
+    % Ji - interior variable, closest to Jb.
     Jb = J(k);
-    Ji = shift_index(sz, Jb, -delta);
+    Ji = shift_index(sz, Jb, -P);
+    % (Xc, Yc) is the midpoint between boundary and the interior points.
     Xc = (X(Jb) + X(Ji))/2;
     Yc = (Y(Jb) + Y(Ji))/2;
+    % We use gradient at (Xc, Yc) to approximate dU = U(Jb) - U(Ji):
     dU = Ux(Xc, Yc) .* (X(Jb) - X(Ji)) + ...
          Uy(Xc, Yc) .* (Y(Jb) - Y(Ji));
-    I = find( A(:, Jb) ); % corresponding equations
+    % NOTE: this approximation is accurate for quadratic U.    
+    I = find( A(:, Jb) ); % corresponding equations for U(Jb).
     F(I) = F(I) - A(I, Jb) * dU; % update RHS
     A(I, Ji) = A(I, Ji) + A(I, Jb); % update LHS
     A(I, Jb) = 0; % Eliminate this variable from A.
