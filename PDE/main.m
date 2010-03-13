32d1117621c1785f4515fae021e7819136fd68fe
Roman Zeyde <roman.zeyde@gmail.com>
2010-03-13 21:42:50 +0200

    Make sure that RHS is eliminated too.

--- PDE/main.m
+++ PDE/main.m
@@ -57,6 +57,7 @@ end
 % Verify that boundary variables are eliminated properly.
 assert(nnz(A(~I, :)) == 0); 
 assert(nnz(A(:, ~I)) == 0); 
+assert(nnz(f(~I)) == 0);
 % Verify that all entries are valid real numbers:
 assert(nnz(isnan(A) | isinf(A)) == 0)
 % Verify that the matrix is symmetric (the original operator is self-adjoint):
