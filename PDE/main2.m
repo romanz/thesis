3f75e8c399b8afba38ed594caa09aeda59c4e3e2
Roman Zeyde <roman.zeyde@gmail.com>
2010-03-22 12:20:55 +0200

    Gaussian example.

--- PDE/main2.m
+++ PDE/main2.m
@@ -1,18 +1,19 @@
-fprintf('\n');
+fprintf('\n'); 
+clear;
 %% Simulate and solve Diffusion-Advection Equation on a 2D grid.
 % The equation to solve is: D Laplacian(u) - V*Gradient(u) = f.
 % Central difference is used for Laplacian. 
 % Upstream difference is used for Gradient.
 % Jacobi iteration is used for relaxation.
 randn('state', 1);
-iters = 15e3;
+iters = 10e3;
 iter_type = 'RedBlack';
-sz = 1+2.^([1 1]*6);
+sz = 1+2.^([1 1]*5);
 
 %% Create the grid
 N = prod(sz);
-x = linspace(-1, 1, sz(1));
-y = linspace(-1, 1, sz(2));
+x = 2*linspace(-1, 1, sz(1));
+y = 2*linspace(-1, 1, sz(2));
 [X, Y] = ndgrid(x, y);
 
 %% The discrete Laplacian operator
@@ -20,8 +21,8 @@ y = linspace(-1, 1, sz(2));
 Diffusion = dinv(M) * L;
 
 %% Velocity field (Vx, Vy) definition on a staggered grid
-Vx =     average(Y, [1; 1]/2); % Evaluated at left and right edges.
-Vy =     average(X, [1, 1]/2); % Evaluated at top and bottom edges.
+Vx = average( Y, [1; 1]/2); % Evaluated at left and right edges.
+Vy = average(-X, [1, 1]/2); % Evaluated at top and bottom edges.
 if sz(2) > 1 && numel(Vx)
     Vx = average(Vx, [0, 1, 0]); % Remove Y boundary
 end
@@ -30,12 +31,12 @@ if sz(1) > 1 && numel(Vy)
 end
 
 %% The discrete advection operator
-Advection = gradient(sz, X, Y, Vx, Vy, 'upstream');
+Advection = gradient(sz, X, Y, Vx, Vy, 'centered');
 
 %% The operator combined operator
 A = Diffusion - Advection; 
-U = @(X, Y) X.^2 + Y.^2;
-f = -4*X .* Y + 4*ones(sz); % Right-Hand Side
+U = @(X, Y) exp(-(X.^2 + Y.^2) / 2);
+f = (X.^2 + Y.^2 - 2) .* exp(-(X.^2 + Y.^2) / 2); % Right-Hand Side
 f(~I) = NaN; % Not defined on the boundary
 
 %% Substitute Boundary Conditions
@@ -53,31 +54,32 @@ assert(nnz(isnan(f(I))) == 0);
 A = A(I, I); 
 f = f(I);
 
-%% Apply relaxation to solve the problem
-fprintf('Construct Jacobi iteration... '); tic;
-[R, T, d] = jacobi(A, f); fprintf('(%.3fs)\n', toc);
-fprintf('Maximal eigenvalue of T: ');
-lambda = abs(max_eigs(T, 1));
-fprintf('%.6f => %d iterations/decade\n', ...
-    lambda, ceil(-1/log10(lambda)));
-
-%% Iteration phase:
-Ui = randn(nnz(I), 1); % Initial guess.
-
-fprintf('Apply %s solver [%d]... ', iter_type, iters); tic;
-if any(strcmpi(iter_type, {'MPE', 'RRE'}))
-    cycle = 20; % Actually each iteration computes (cycle + 1) vectors.
-    iters = iters / cycle;
-    [Uf, residuals] = extrapolate(Ui, @(u) T*u + d, cycle, iters, iter_type);
+%% Solve the linear system
+if isempty(iter_type) % exact solver
+    Uf = A \ f(:);
 else
-    [Uf, residuals] = iterate(Ui, T, d, iters, iter_type); 
+% Apply relaxation to solve the problem
+    fprintf('Construct Jacobi iteration... '); tic;
+    [R, T, d] = jacobi(A, f); fprintf('(%.3fs)\n', toc);
+
+    % Iteration phase:
+    Ui = randn(nnz(I), 1); % Initial guess.
+
+    fprintf('Apply %s solver [%d]... ', iter_type, iters); tic;
+    if any(strcmpi(iter_type, {'MPE', 'RRE'}))
+        cycle = 20; % Actually each iteration computes (cycle + 1) vectors.
+        iters = iters / cycle;
+        [Uf, residuals] = extrapolate(Ui, @(u) T*u + d, cycle, iters, iter_type);
+    else
+        [Uf, residuals] = iterate(Ui, T, d, iters, iter_type); 
+    end
 end
 fprintf('(%.3fs)\n', toc);
-
 
 %% Save and show the results.
 figure(1); clf;
 mat_file = 'results.mat';
-save(mat_file)
-err = show(mat_file); 
+delete(mat_file);
+save(mat_file);
+err = show(load(mat_file)); 
 fprintf('error: %e\n', err);
--- PDE/show.m
+++ PDE/show.m
@@ -1,26 +1,34 @@
-function [e] = show(mat_file)
+function [e] = show(S)
 %% Show solver's results
-load(mat_file)
-U0 = U(X, Y); % The original continuous solution
+U0 = S.U(S.X, S.Y); % The original continuous solution
 U = U0;
-U(I) = Uf;
+U(S.I) = S.Uf;
+clf;
 
 subplot 131; 
-visualize(X, Y, U, sprintf('Solution (%d iterations)', iters));
+if ~isempty(S.iter_type)
+    s = sprintf('(%d iterations)', S.iters);
+else
+    s = '(Direct)';
+end
+visualize(sprintf(['Solution ' s]), S.X, S.Y, U, U0);
 
 subplot 132; 
 E = U0 - U;
 e = norm(E(:), inf);
-visualize(X, Y, E, sprintf('Error (L_\\infty = %.3e)', e))
+visualize(sprintf('Error (L_\\infty = %.3e)', e), S.X, S.Y, E)
 
-subplot 133; 
-semilogy(1:numel(residuals), residuals);
-stop_iter = find(residuals, 1, 'last');
-title(sprintf('Residual L_2 norm\n(after %d %s iterations)', ...
-    stop_iter, iter_type)); 
-xlabel('Iteration #');
+if isfield(S, 'residuals')
+    subplot 133;
+    semilogy(1:numel(S.residuals), S.residuals);
+    stop_iter = find(S.residuals, 1, 'last');
+    title(sprintf('Residual L_2 norm\n(after %d %s iterations)', ...
+        stop_iter, S.iter_type)); 
+    xlabel('Iteration #');
+end
 
-function visualize(X, Y, Z, t)
+function visualize(msg, X, Y, varargin)
+Z = varargin{1};
 sz = size(Z).';
 if sz(1) > 1 && sz(2) > 1
     surf(X, Y, Z, 'EdgeColor', 'Black');  
@@ -28,6 +36,14 @@ if sz(1) > 1 && sz(2) > 1
     ylabel('Y');
 else
     XY = [X(:) Y(:)]; % 1D plot hack
-    plot(XY(:, sz > 1), Z(:), '-'); 
+    t = XY(:, sz > 1);
+    plot_func = @plot;
+    fmt = {'b-', 'r-.'};
+    hold on;
+    for k = 1:numel(varargin)
+        Z = varargin{k};
+        plot_func(t, Z(:), fmt{k}); 
+    end
+    hold off;
 end
-title(t);
+title(msg);
