52187d2a8f18953d915dbf8ccb4b2c91d4a897e2
Roman Zeyde <roman.zeyde@gmail.com>
2010-04-08 17:33:06 +0300

    Add DLL support

--- extrapolation/C/extrapolation.c
+++ extrapolation/C/extrapolation.c
@@ -1,5 +1,11 @@
 #include <math.h>
 
+#ifdef BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT __declspec(dllimport)
+#endif
+
 typedef double real_t;
 typedef unsigned long int size_t;
 
@@ -16,11 +22,11 @@ real_t dot(const real_t *v, const real_t *u, size_t dim)
 real_t normalize(real_t *v, size_t dim)
 {
     real_t norm = sqrt(dot(v, v, dim));
-    
-    real_t inv_norm = 1.0 / norm;    
+
+    real_t inv_norm = 1.0 / norm;
     for (size_t k = 0; k < dim; ++k)
         v[k] = v[k] * inv_norm;
-        
+
     return norm;
 }
 
@@ -36,15 +42,15 @@ void mgs(real_t *vectors, real_t *R, size_t dim, size_t num)
 {
     for (size_t k = 0; k < num * num; ++k)
         R[k] = 0;
-        
+
     for (size_t i = 0; i < num; ++i) {
         real_t *v = vectors + i * dim;
 
         for (size_t j = 0; j < i; ++j) {
-            
+
             real_t *u = vectors + j * dim;
             real_t r = dot(v, u, dim);
-            
+
             for (size_t k = 0; k < dim; ++k)
                 v[k] = v[k] - r * u[k];
 
@@ -54,7 +60,7 @@ void mgs(real_t *vectors, real_t *R, size_t dim, size_t num)
     }
 }
 
-int extrapolate(real_t *vectors, size_t dim, size_t num, int method)
+int DLL_EXPORT extrapolate(real_t *vectors, size_t dim, size_t num, int method)
 {
     real_t *const v0 = vectors;
 
@@ -71,7 +77,7 @@ int extrapolate(real_t *vectors, size_t dim, size_t num, int method)
     /* QR decomposition */
     real_t R[num * num];
     mgs(vectors, R, dim, num);
-    
+
     /* Compute coefficients */
     real_t gamma[num];
     switch (method) {
@@ -79,7 +85,7 @@ int extrapolate(real_t *vectors, size_t dim, size_t num, int method)
         case RRE:  	rre(R, gamma, num); break;
         default:    return -1;
     }
-    
+
 	/* xi = 1 - cumsum(gamma) */
     for (size_t i = 1; i < num; ++i)
     	gamma[i] = gamma[i] + gamma[i-1];
@@ -91,7 +97,7 @@ int extrapolate(real_t *vectors, size_t dim, size_t num, int method)
 	real_t eta[num];
     for (size_t i = 0; i < num; ++i)
     	eta[i] = dot(R + i*num, xi, num);
-    
+
     /* Compute the solution: v0 + Q * eta */
     for (size_t i = 0; i < num; ++i) {
         real_t *q = vectors + i * dim;
