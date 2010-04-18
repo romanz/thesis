265ecd640b54e597cd7b768e543a95fa5246947a
Roman Zeyde <roman.zeyde@gmail.com>
2010-04-18 14:03:18 +0300

    Dimension bug fix

--- PDE/main3.m
+++ PDE/main3.m
@@ -5,14 +5,15 @@ sz = [6 6];
 
 Fx = ones(sz - [3 2]);
 Fy = ones(sz - [2 3]);
-Vx = ones(sz(1)-2, 2);
-Vy = ones(sz(2)-2, 2);
-[A, f, Mr, Mb] = stokes(sz, X, Y, Fx(:), Fy(:), Vx, Vy);
+[A, f, Mr, Mb] = stokes(sz, X, Y, Fx(:), Fy(:), ...
+    ones(sz(2)-2, 2), ones(sz(1)-2, 2));
 size(A)
 x = randn(numel(f), 1);
 for k = 1:1000
     x = x + Mr*(f-A*x); 
     x = x + Mb*(f-A*x);
 end
-% x(1:(numel(Fx)+numel(Fy)))
-x
+Vx = reshape(x(1:numel(Fx)), sz - [3 2])
+Vy = reshape(x((1:numel(Fy)) + numel(Fx)), sz - [2 3])
+P = reshape(x((1 + numel(Fx) + numel(Fy)):end), sz - 2);
+P = P - P(1)
