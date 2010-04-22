7751aad5209214064868dd8fbb745621455f1200
Roman Zeyde <roman.zeyde@gmail.com>
2010-04-22 22:08:03 +0300

    Refactor.

--- PDE/main3.m
+++ PDE/main3.m
@@ -1,14 +1,23 @@
 function main3
 clc
-sz = [5 5];
+sz = [1 1]*4;
 [X, Y] = ndgrid(1:sz(1), 1:sz(2));
 
-Fx = ones(sz - [3 2]);
-Fy = ones(sz - [2 3]);
+% Vx
+[Xx, Yx, Lx, Rx, Dx, Ux, Ix] = staggered_boundary(sz, X, Y, 1);
+Fx = @(x, y) 0*x+0*y+1;
+Fx = Fx(Xx(Ix), Yx(Ix));
 
-[A, f, Mr, Mb] = stokes(sz, X, Y, Fx, Fy, ...
+% Vy
+[Xy, Yy, Ly, Ry, Dy, Uy, Iy] = staggered_boundary(sz, X, Y, 2);
+Fy = @(x, y) 0*x+0*y+1;
+Fy = Fy(Xy(Iy), Yy(Iy));
+
+[A, f] = stokes(sz, X, Y, Fx, Fy, ...
     ones(sz(2)-2, 2), ones(sz(1)-2, 2));
-size(A)
+[Mr, Mb] = stokes_vanka_redblack(sz, A);
+
+% size(A)
 x = randn(numel(f), 1);
 for k = 1:100
     x = x + Mr*(f-A*x); 
@@ -22,3 +31,13 @@ P = P - P(1)
 norm(f - A*x, inf)
 
 save results
+end
+
+function [X, Y, L, R, D, U, I] = staggered_boundary(sz, X, Y, dim)
+    [X, Y, P] = staggered_grid(sz, X, Y, dim);
+    I = interior(sz - P);
+    L = ~I & shift(I, [-1, 0]);
+    R = ~I & shift(I, [+1, 0]);
+    D = ~I & shift(I, [0, -1]);
+    U = ~I & shift(I, [0, +1]);
+end
--- PDE/stokes.m
+++ PDE/stokes.m
@@ -1,6 +1,6 @@
 % 2D Stokes equation discretization for specific dimension
 % {X,Y} are cell centers coordinates (incl. the boundary cells)
-function [A, f, Mr, Mb] = stokes(sz, X, Y, Fx, Fy, Vx, Vy)
+function [A, f] = stokes(sz, X, Y, Fx, Fy, Vx, Vy)
     n = prod(sz - 2); % # of interior cells
     f = zeros(n, 1); % Zero Flux Condition
 
@@ -17,7 +17,6 @@ function [A, f, Mr, Mb] = stokes(sz, X, Y, Fx, Fy, Vx, Vy)
          Gx_vx, Gy_vy, sparse(n, n)];
 
     f = [Fx; Fy; f];    
-    [Mr, Mb] = redblack(sz, A);    
 end
 
 function [A, f] = elim(dim, sz, A, f, V)
@@ -37,38 +36,4 @@ function [J, I] = find_boundary(interior, P)
     [J, I] = boundary(interior, P);
     J = find(J);
     I = find(I);
-end
-
-function [M1, M2] = redblack(sz, A)
-    sz = sz - 2;
-    K = 1:prod(sz);
-    K = K(:);
-    [I, J] = ind2sub(sz, K);
-    
-    % Same indices for variables and equations.
-    V = [[index(sz - [1 0], I-1, J), index(sz - [1 0], I, J)], ...
-         [index(sz - [0 1], I, J-1), index(sz - [0 1], I, J)] + prod(sz - [1 0]), ...
-         K + prod(sz - [1 0]) + prod(sz - [0 1])];
-    
-    K = logical(mod(I - J, 2)); 
-    V1 = arr2cell(V( K, :)); % odd
-    V2 = arr2cell(V(~K, :)); % even
-    
-    M1 = vanka(A, V1, V1);
-    M2 = vanka(A, V2, V2);
-end
-
-function C = arr2cell(A)
-    C = cell(size(A, 1), 1);
-    for k = 1:numel(C)
-        a = A(k, :);
-        a = a(~isnan(a));
-        C{k} = a(:);
-    end
-end
-
-function K = index(sz, I, J)
-    Q = (1 <= I) & (I <= sz(1)) & (1 <= J) & (J <= sz(2));
-    K = nan(size(Q));
-    K(Q) = sub2ind(sz, I(Q), J(Q));
 end
