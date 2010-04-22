8e347e2f571de8d28e131c935b98a9685117eff0
Roman Zeyde <roman.zeyde@gmail.com>
2010-04-22 15:42:16 +0300

    Implement shift without wraparound

--- PDE/shift.m
+++ PDE/shift.m
@@ -1,2 +1,19 @@
-function I = shift(I, P)
-I = circshift(I, P);
+function I = shift(I, Q)
+
+sz = size(I);
+Qp = max(Q, 0); % positive shifts
+Qn = min(Q, 0); % negative shifts
+
+I = [zeroes(Q(1), size(I, 2)); ...
+     I(1-Qn(1):end-Qp(1), :); ...
+     zeroes(-Q(1), size(I, 2))];
+
+I = [zeroes(size(I, 1), Q(2)), ...
+     I(:, 1-Qn(2):end-Qp(2)), ...
+     zeroes(size(I, 1), -Q(2))];
+
+function Z = zeroes(n, m)
+    Z = zeros(min(max([n m], 0), sz));
+end
+
+end
