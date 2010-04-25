98704df4527f42c64943c3e676b5127d037cc2ed
Roman Zeyde <roman.zeyde@gmail.com>
2010-04-25 19:10:21 +0300

    Fix dimensions.

--- PDE/main3.m
+++ PDE/main3.m
@@ -1,12 +1,13 @@
 function main3
 clc
-sz = [1 1]*4;
+iters = 40;
+sz = [1 1]*2^2;
 [X, Y] = ndgrid(1:sz(1), 1:sz(2));
 
 % Vx
 Vx = @(X, Y) 1 + 0*X.^2 .* Y;
 [Xx, Yx, Lx, Rx, Dx, Ux, Ix, Ex] = staggered_boundary(sz, X, Y, 1);
-Fx = @(x, y) 0*x+0*y+1;
+Fx = @(x, y) 0*x+0*y+0;
 Fx = Fx(Xx(Ix), Yx(Ix));
 Jx = [find(Lx); find(Rx)];
 Wx = 1+0*Jx;
@@ -15,14 +16,14 @@ ux = Vx(Xx(Jx), Yx(Jx));
 Jx = [Jx * [1 0]; ...
      [find(Dx), find(shift(Dx, [0, +1]))]; ...
      [find(Ux), find(shift(Ux, [0, -1]))]; find(Ex) * [1 0]]; 
-Wx = [Wx * [1 0]; repmat([-1, 1], nnz(Dx)); repmat([1, -1], nnz(Ux)); ...
+Wx = [Wx * [1 0]; repmat([-1, 1], nnz(Dx), 1); repmat([1, -1], nnz(Ux), 1); ...
       ones(nnz(Ex), 2)];
 ux = [ux; 0*find(Dx); 0*find(Ux); zeros(nnz(Ex), 1)];
 
 % Vy
 Vy = @(X, Y) 1 - 0*Y.^2 .* X;
 [Xy, Yy, Ly, Ry, Dy, Uy, Iy, Ey] = staggered_boundary(sz, X, Y, 2);
-Fy = @(x, y) 0*x+0*y+1;
+Fy = @(x, y) 0*x+0*y+0;
 Fy = Fy(Xy(Iy), Yy(Iy));
 Jy = [find(Dy); find(Uy)];
 Wy = 1+0*Jy;
@@ -31,7 +32,7 @@ uy = Vy(Xy(Jy), Yy(Jy));
 Jy = [Jy * [1 0]; ...
      [find(Ly), find(shift(Ly, [+1, 0]))]; ...
      [find(Ry), find(shift(Ry, [-1, 0]))]; find(Ey) * [1 0]]; 
-Wy = [Wy * [1 0]; repmat([-1, 1], nnz(Ly)); repmat([1, -1], nnz(Ry)); ...
+Wy = [Wy * [1 0]; repmat([-1, 1], nnz(Ly), 1); repmat([1, -1], nnz(Ry), 1); ...
       ones(nnz(Ey), 2)];
 uy = [uy; 0*find(Ly); 0*find(Ry); zeros(nnz(Ey), 1)];
 
@@ -56,16 +57,24 @@ f = [Fx+fx; Fy+fy; f1+f2];
 [Mr, Mb] = stokes_vanka_redblack(sz, A);
 
 x = randn(numel(f), 1);
-for k = 1:100
-    x = x + Mr*(f-A*x); 
-    x = x + Mb*(f-A*x);
+res = zeros(iters, 1);
+for k = 1:iters
+    r = f - A*x;
+    x = x + Mr*r; 
+
+    r = f - A*x;
+    x = x + Mb*r;
+
+    res(k) = norm(r, 2);
 end
 Vx = reshape(x(1:numel(Fx)), sz - [3 2])
 Vy = reshape(x((1:numel(Fy)) + numel(Fx)), sz - [2 3])
 P = reshape(x((1 + numel(Fx) + numel(Fy)):end), sz - 2);
 P = P - P(1)
 
-norm(f - A*x, inf)
+clf;
+semilogy(res); 
+axis([0 iters 1e-16 100])
 
 save results
 end
