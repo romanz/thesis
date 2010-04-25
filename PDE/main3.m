3f13f9e4341283b5c154814f7c2c3c5a2895df4a
Roman Zeyde <roman.zeyde@gmail.com>
2010-04-25 21:20:11 +0300

    Test case for Stokes.

--- PDE/main3.m
+++ PDE/main3.m
@@ -1,45 +1,62 @@
 function main3
 clc
-iters = 40;
-sz = [1 1]*2^2;
+iters = 400;
+sz = [5 4];
 [X, Y] = ndgrid(1:sz(1), 1:sz(2));
+Vx = @(X, Y)  (X.^2).*Y;
+Vy = @(X, Y) -X.*(Y.^2);
+Fx = @(x, y) -2*y;
+Fy = @(x, y)  2*x;
 
-% Vx
-Vx = @(X, Y) 1 + 0*X.^2 .* Y;
+%% Vx
 [Xx, Yx, Lx, Rx, Dx, Ux, Ix, Ex] = staggered_boundary(sz, X, Y, 1);
-Fx = @(x, y) 0*x+0*y+0;
+shave(Vx(Xx, Yx), 1, 1)
 Fx = Fx(Xx(Ix), Yx(Ix));
 Jx = [find(Lx); find(Rx)];
-Wx = 1+0*Jx;
+Wx = ones(nnz(Jx), 1);
 ux = Vx(Xx(Jx), Yx(Jx));
 
+Dx1 = shift(Dx, [0, +1]);
+Ux1 = shift(Ux, [0, -1]);
 Jx = [Jx * [1 0]; ...
-     [find(Dx), find(shift(Dx, [0, +1]))]; ...
-     [find(Ux), find(shift(Ux, [0, -1]))]; find(Ex) * [1 0]]; 
-Wx = [Wx * [1 0]; repmat([-1, 1], nnz(Dx), 1); repmat([1, -1], nnz(Ux), 1); ...
-      ones(nnz(Ex), 2)];
-ux = [ux; 0*find(Dx); 0*find(Ux); zeros(nnz(Ex), 1)];
+     [find(Dx), find(Dx1)]; ...
+     [find(Ux), find(Ux1)]; ...
+     find(Ex) * [1 0]]; 
+Wx = [Wx * [1 0]; ...
+      repmat([1, -1], nnz(Dx), 1); ...
+      repmat([1, -1], nnz(Ux), 1); ...
+      ones(nnz(Ex), 1) * [1 0]];
+ux = [ux; ...
+    Vx(Xx(Dx), Yx(Dx)) - Vx(Xx(Dx1), Yx(Dx1)); ...
+    Vx(Xx(Ux), Yx(Ux)) - Vx(Xx(Ux1), Yx(Ux1)); ...
+    zeros(nnz(Ex), 1)];
 
-% Vy
-Vy = @(X, Y) 1 - 0*Y.^2 .* X;
+%% Vy
 [Xy, Yy, Ly, Ry, Dy, Uy, Iy, Ey] = staggered_boundary(sz, X, Y, 2);
-Fy = @(x, y) 0*x+0*y+0;
+shave(Vy(Xy, Yy), 1, 1)
 Fy = Fy(Xy(Iy), Yy(Iy));
 Jy = [find(Dy); find(Uy)];
-Wy = 1+0*Jy;
+Wy = ones(nnz(Jy), 1);
 uy = Vy(Xy(Jy), Yy(Jy));
 
-Jy = [Jy * [1 0]; ...
-     [find(Ly), find(shift(Ly, [+1, 0]))]; ...
-     [find(Ry), find(shift(Ry, [-1, 0]))]; find(Ey) * [1 0]]; 
-Wy = [Wy * [1 0]; repmat([-1, 1], nnz(Ly), 1); repmat([1, -1], nnz(Ry), 1); ...
-      ones(nnz(Ey), 2)];
-uy = [uy; 0*find(Ly); 0*find(Ry); zeros(nnz(Ey), 1)];
+Ly1 = shift(Ly, [+1, 0]);
+Ry1 = shift(Ry, [-1, 0]);
+Jy = [Jy * [1 0]; 
+     [find(Ly), find(Ly1)]; ...
+     [find(Ry), find(Ry1)]; ...
+     find(Ey) * [1 0]]; 
+Wy = [Wy * [1 0]; ...
+      repmat([1, -1], nnz(Ly), 1); ...
+      repmat([1, -1], nnz(Ry), 1); ...
+      ones(nnz(Ey), 1) * [1 0]];
+uy = [uy; ...
+    Vy(Xy(Ly), Yy(Ly)) - Vy(Xy(Ly1), Yy(Ly1)); ...
+    Vy(Xy(Ry), Yy(Ry)) - Vy(Xy(Ry1), Yy(Ry1)); ...
+    zeros(nnz(Ey), 1)];
 
+%% Build equations
 [Gx_p, L_vx, Gx_vx] = stokes1(sz, X, Y, 1);
 [Gy_p, L_vy, Gy_vy] = stokes1(sz, X, Y, 2);
-
-n = prod(sz - 2); % # of interior cells
 
 [fx, L_vx ] = subst(-L_vx, Jx, Wx, ux);
 [f1, Gx_vx] = subst(Gx_vx, Jx, Wx, ux);
@@ -47,11 +64,12 @@ n = prod(sz - 2); % # of interior cells
 [fy, L_vy ] = subst(-L_vy, Jy, Wy, uy);
 [f2, Gy_vy] = subst(Gy_vy, Jy, Wy, uy);
 
+n = prod(sz - 2); % # of interior cells
 A = [L_vx, sparse(size(L_vx, 1), size(L_vy, 2)), Gx_p; ...
      sparse(size(L_vy, 1), size(L_vx, 2)), L_vy, Gy_p; ...
      Gx_vx, Gy_vy, sparse(n, n)];
 
-f = [Fx+fx; Fy+fy; f1+f2];    
+f = [Fx+fx; Fy+fy; f1+f2];
 
 %
 [Mr, Mb] = stokes_vanka_redblack(sz, A);
@@ -70,7 +88,7 @@ end
 Vx = reshape(x(1:numel(Fx)), sz - [3 2])
 Vy = reshape(x((1:numel(Fy)) + numel(Fx)), sz - [2 3])
 P = reshape(x((1 + numel(Fx) + numel(Fy)):end), sz - 2);
-P = P - P(1)
+P = P - P(1) + 0
 
 clf;
 semilogy(res); 
