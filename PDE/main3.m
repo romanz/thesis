416010698dc40a299885b8a31bea32ac4fc03b1f
Roman Zeyde <roman.zeyde@gmail.com>
2010-04-29 19:54:14 +0300

    Bug fix - cell interior points must be at the center of each cell.

--- PDE/main3.m
+++ PDE/main3.m
@@ -1,18 +1,20 @@
 function main3
 clc
 iters = 100;
-sz = [1 1]*6;
-[X, Y] = ndgrid((1:sz(1))*2, (1:sz(2))*2);
+x = [-1 -.3 2 3]';
+y = [-2 .2 1];
 Vx  = @(x, y)  y.* x.^2;
 Vy  = @(x, y) -x.* y.^2;
 Fx  = @(x, y) -2*y;
 Fy  = @(x, y)  2*x;
 div = @(x, y)  0*x + 0*y;
 
+[sz, Xc, Yc, Xx, Yx, Xy, Yy] = create_grids(x, y);
+
 %% Vx
-[Xx, Yx, Lx, Rx, Dx, Ux, Ix, Ex] = staggered_boundary(sz, X, Y, 1);
+[Lx, Rx, Dx, Ux, Ix, Ex] = create_boundaries(sz + [0 1]);
+shave(Vx(Xx, Yx), 1, 1) % display correct Vx
 Fx = Fx(Xx(Ix), Yx(Ix));
-shave(Vx(Xx, Yx), 1, 1)
 Jx = [find(Lx); find(Rx)];
 Wx = ones(nnz(Jx), 1);
 ux = Vx(Xx(Jx), Yx(Jx));
@@ -33,8 +35,8 @@ ux = [ux; ...
     zeros(nnz(Ex), 1)];
 
 %% Vy
-[Xy, Yy, Ly, Ry, Dy, Uy, Iy, Ey] = staggered_boundary(sz, X, Y, 2);
-shave(Vy(Xy, Yy), 1, 1)
+[Ly, Ry, Dy, Uy, Iy, Ey] = create_boundaries(sz + [1 0]);
+shave(Vy(Xy, Yy), 1, 1) % display correct Vy
 Fy = Fy(Xy(Iy), Yy(Iy));
 Jy = [find(Dy); find(Uy)];
 Wy = ones(nnz(Jy), 1);
@@ -56,8 +58,8 @@ uy = [uy; ...
     zeros(nnz(Ey), 1)];
 
 %% Build equations
-[Gx_p, L_vx, Gx_vx] = stokes1(sz, X, Y, 1);
-[Gy_p, L_vy, Gy_vy] = stokes1(sz, X, Y, 2);
+[Gx_p, L_vx, Gx_vx] = stokes1(1, Xx, Yx, Xc, Yc);
+[Gy_p, L_vy, Gy_vy] = stokes1(2, Xy, Yy, Xc, Yc);
 
 [fx, L_vx ] = subst(-L_vx, Jx, Wx, ux);
 [f1, Gx_vx] = subst(Gx_vx, Jx, Wx, ux);
@@ -65,16 +67,16 @@ uy = [uy; ...
 [fy, L_vy ] = subst(-L_vy, Jy, Wy, uy);
 [f2, Gy_vy] = subst(Gy_vy, Jy, Wy, uy);
 
-n = prod(sz - 2); % # of interior cells
+% CR: sz hack!
+n = prod(sz - 1); % # of interior cells
 A = [L_vx, sparse(size(L_vx, 1), size(L_vy, 2)), Gx_p; ...
      sparse(size(L_vy, 1), size(L_vx, 2)), L_vy, Gy_p; ...
      Gx_vx, Gy_vy, sparse(n, n)];
 
-I = interior(sz);
-f = [Fx+fx; Fy+fy; f1+f2+div(X(I), Y(I))];
+f = [Fx+fx; Fy+fy; f1+f2+div(Xc(:), Yc(:))];
 
-%
-[Mr, Mb] = stokes_vanka_redblack(sz, A);
+% CR: sz hack!
+[Mr, Mb] = stokes_vanka_redblack(sz+1, A);
 
 x = randn(numel(f), 1);
 res = zeros(iters, 1);
@@ -83,9 +85,11 @@ for k = 1:iters
     r = f - A*x;    x = x + Mb*r;
     res(k) = norm(r, 2);
 end
-Vx = reshape(x(1:numel(Fx)), sz - [3 2])
-Vy = reshape(x((1:numel(Fy)) + numel(Fx)), sz - [2 3])
-P = reshape(x((1 + numel(Fx) + numel(Fy)):end), sz - 2);
+% CR: sz hack!
+sz = sz - 1;
+Vx = reshape(x(1:numel(Fx)), sz - [1 0])
+Vy = reshape(x((1:numel(Fy)) + numel(Fx)), sz - [0 1])
+P = reshape(x((1 + numel(Fx) + numel(Fy)):end), sz);
 P = P - P(1) + 0
 
 clf;
@@ -95,12 +99,29 @@ axis([0 iters 1e-16 100])
 save results
 end
 
-function [X, Y, L, R, D, U, I, E] = staggered_boundary(sz, X, Y, dim)
-    [X, Y, P] = staggered_grid(sz, X, Y, dim);
-    I = interior(sz - P);
+function [L, R, D, U, I, E] = create_boundaries(sz)
+    I = interior(sz);
     L = ~I & shift(I, [-1, 0]);
     R = ~I & shift(I, [+1, 0]);
     D = ~I & shift(I, [0, -1]);
     U = ~I & shift(I, [0, +1]);
     E = ~(I | L | R | D | U);
+end
+
+function [sz, cX, cY, xX, xY, yX, yY] = create_grids(x, y)
+    x = x(:);
+    y = y(:);
+    sz = [numel(x) numel(y)];
+
+    % extended grid (for ghost points)
+    x = [2*x(1) - x(2); x; 2*x(end) - x(end-1)];
+    y = [2*y(1) - y(2); y; 2*y(end) - y(end-1)];
+
+    % cell-centered coordinates
+    xc = average(x, [1; 1]/2);
+    yc = average(y, [1; 1]/2);
+
+    [cX, cY] = ndgrid(xc(2:end-1), yc(2:end-1));
+    [xX, xY] = ndgrid(x(2:end-1), yc);
+    [yX, yY] = ndgrid(xc, y(2:end-1));
 end
--- PDE/stokes1.m
+++ PDE/stokes1.m
@@ -1,17 +1,16 @@
 % Construct Stokes equation for specified dimension.
-function [Gp, Lv, Gv] = stokes1(sz, X, Y, dim)
-    [Xs, Ys, dir] = staggered_grid(sz, X, Y, dim); 
-    I = interior(sz - dir); 
+function [Gp, Lv, Gv] = stokes1(dim, Xs, Ys, X, Y)
+    sz = size(0*Xs + 0*Ys);
+    I = interior(sz);
     Lv = laplacian(I, Xs, Ys);
     Lv = Lv(I, :);
     % full(Lv)
-
+    dir = (1:numel(sz)) == dim;
     K = I | shift(I, -dir); % add first row/column
     Gv = gradient(K, Xs, Ys, dir);
     % full(Gv)
-
-    I = interior(sz);
+    I = true(size(0*X+0*Y));
     K = I & shift(I, -dir); % remove last row/column
-    Gp = gradient(shave(K, 1, 1), X(I), Y(I), dir);
+    Gp = gradient(K, X, Y, dir);
     % full(Gp)
 end
