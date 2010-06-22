e18acbb5b47b6dd0615083d1763e92b1be83bd33
Roman Zeyde <roman.zeyde@gmail.com>
2010-06-22 14:27:47 +0300

    stokes uses divergence

--- ng/stokes1.m
+++ ng/stokes1.m
@@ -4,7 +4,7 @@ function [Gp, Lv, Gv] = stokes1(dim, gridV, gridP)
     % full(Lv)
     dir = (1:numel(gridP.sz)) == dim;
     K = gridV.I | shift(gridV.I, -dir); % add first row/column
-    Gv = gradient(K, gridV.X, gridV.Y, dir);
+    Gv = divergence(K, gridV.X, gridV.Y, dir);
     % full(Gv)
     K = gridP.I & shift(gridP.I, -dir); % remove last row/column
     Gp = gradient(K, gridP.X, gridP.Y, dir);
