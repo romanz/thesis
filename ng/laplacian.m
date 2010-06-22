8a0515519d3f8a4ad8338d542d21996bf1bdc812
Roman Zeyde <roman.zeyde@gmail.com>
2010-06-22 16:05:21 +0300

    Refactor.

--- ng/laplacian.m
+++ ng/laplacian.m
@@ -6,42 +6,43 @@ if nargin < 4
     C = ones(sz); % Assume uniform C
 end
 
-ind = @(I, J) sub2ind(sz, I, J);
-K = find(interior); % Fill only interior points
-K = K(:);
-[I, J] = ind2sub(sz, K);
-Dp = repmat([1 0 -1], [numel(K) 1]); 
+% Fill only interior points
+M = nnz(interior);
+[I, J] = ind2sub(sz, find(interior));
+Dp = repmat([1 0 -1], [M 1]); 
 % NOTE: The stencil is constructed as: [forward, middle, backward] coefficients.
 Ip = repmat(I, [1 3]);
 Jp = repmat(J, [1 3]);
-Kp = repmat((1:numel(K))', [1 3]); % interior variables' indices, for 1D Laplacian stencil
+Kp = repmat((1:M)', [1 3]); % interior variables' indices, for 1D Laplacian stencil
 
 % Build 2D sparse matrix L = "Dxx/Mxx" + "Dyy/Myy":
+K = interior;
+Kr = shift(K, [+1 0]); % Left
+Kl = shift(K, [-1 0]); % Right
+Ku = shift(K, [0 +1]); % Up
+Kd = shift(K, [0 -1]); % Down
+ind = @(I, J) sub2ind(sz, I, J);
 
 % for X
-    Kr = ind(I+1, J); % Left
-    Kl = ind(I-1, J); % Right
     Dxx = ... % Laplacian stencil in X direction
-     col(( C(Kr) + C(K) ) ./ (( X(Kr) - X(K) ))) * [1 -1 0] -  ...
-     col(( C(K) + C(Kl) ) ./ (( X(K) - X(Kl) ))) * [0 1 -1];
+     (( C(Kr) + C(K) ) ./ (( X(Kr) - X(K) ))) * [1 -1 0] -  ...
+     (( C(K) + C(Kl) ) ./ (( X(K) - X(Kl) ))) * [0 1 -1];
     P = ind(Ip + Dp, Jp); % column indices
-    Dxx = sparse(Kp, P, Dxx, numel(K), N);
+    Dxx = sparse(Kp, P, Dxx, M, N);
     % The denumerator is separated, for A to be symmetric
     % (since the original operator is self-adjoint).
-    Mxx = sparse(1:numel(K), 1:numel(K), (X(Kr) - X(Kl)), numel(K), numel(K));
+    Mxx = sparse(1:M, 1:M, (X(Kr) - X(Kl)), M, M);
 % Laplacian for X direction is (pinv(Mxx) * Dxx)
 
 % for Y
-    Ku = ind(I, J+1); % Up
-    Kd = ind(I, J-1); % Down
     Dyy = ... % Laplacian stencil in Y direction
-     col(( C(Ku) + C(K) ) ./ (( Y(Ku) - Y(K) ))) * [1 -1 0] -  ...
-     col(( C(K) + C(Kd) ) ./ (( Y(K) - Y(Kd) ))) * [0 1 -1];
+     (( C(Ku) + C(K) ) ./ (( Y(Ku) - Y(K) ))) * [1 -1 0] -  ...
+     (( C(K) + C(Kd) ) ./ (( Y(K) - Y(Kd) ))) * [0 1 -1];
     P = ind(Ip, Jp + Dp); % column indices
-    Dyy = sparse(Kp, P, Dyy, numel(K), N);
+    Dyy = sparse(Kp, P, Dyy, M, N);
     % The denumerator is separated, for A to be symmetric
     % (since the original operator is self-adjoint).
-    Myy = sparse(1:numel(K), 1:numel(K), (Y(Ku) - Y(Kd)), numel(K), numel(K));
+    Myy = sparse(1:M, 1:M, (Y(Ku) - Y(Kd)), M, M);
 % Laplacian for Y direction is (pinv(Myy) * Dyy)
 
 % % The actual Laplacian matrix is:
