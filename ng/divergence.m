a7ce683be4559dfd274318851fea711d48ba125f
Roman Zeyde <roman.zeyde@gmail.com>
2010-06-22 18:03:41 +0300

    Refactor.

--- ng/divergence.m
+++ ng/divergence.m
@@ -9,6 +9,6 @@ function G = divergence(K, X, Y, dir)
         case 1, D = X(K2) - X(K1);
         case 2, D = Y(K2) - Y(K1);
     end
-    G = sparse([1:numel(K0), 1:numel(K0)], [K1 K2], 1./[-D D], ...
+    G = sparse([1:numel(K0), 1:numel(K0)], [K1 K2], [-1./D,  1./D], ...
         numel(K0), prod(sz));
 end
