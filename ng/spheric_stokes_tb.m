a3c8008955489ba48e4316f4dd0a26afaf597d7d
Roman Zeyde <roman.zeyde@gmail.com>
2010-07-22 19:12:09 +0300

    refactor

--- ng/spheric_stokes_tb.m
+++ ng/spheric_stokes_tb.m
@@ -1,7 +1,7 @@
 % >> spheric_stokes_tb(400, 30, 100e3);
 
 function spheric_stokes_tb(nx, ny, iters)
-x = logspace(0, 2.3, nx);
+x = logspace(0, 2, nx);
 y = linspace(0, pi, ny);
 % y = ( 1 - cos(linspace(0, pi, ny)) )*pi/2;
 [center, gridP, gridVx, gridVy] = grids(x, y);
@@ -26,18 +26,20 @@ A = spheric_stokes(gridVx, gridVy, gridP);
 % norm(div(:), inf)
 
 %%
-u = [cos(gridVx.Y(:)) .* (1 - 1.5 ./ gridVx.X(:) + 0.5 ./ gridVx.X(:).^3); ...
-    -sin(gridVy.Y(:)) .* (1 - 0.75 ./ gridVy.X(:) - 0.25 ./ gridVy.X(:).^3); ...
-    -1.5 * cos(gridP.Y(:)) ./ gridP.X(:).^2];
-% Somehow, Fx component is not O(h^2).
+Ux0 = cos(gridVx.Y(:)) .* (1 - 1.5 ./ gridVx.X(:) + 0.5 ./ gridVx.X(:).^3); 
+Ux0 = reshape(Ux0, gridVx.sz);
+Uy0 = -sin(gridVy.Y(:)) .* (1 - 0.75 ./ gridVy.X(:) - 0.25 ./ gridVy.X(:).^3); 
+Uy0 = reshape(Uy0, gridVy.sz);
+P0  = -1.5 * cos(gridP.Y(:)) ./ gridP.X(:).^2;
+u = [Ux0(:); Uy0(:); P0(:)];
 
 % u = [gridVx.X(:) .* sin(gridVx.Y(:)) .* sin(gridVx.Y(:)); ...
 %      gridVy.X(:) .* cos(gridVy.Y(:)) .* sin(gridVy.Y(:)); ...
 %     0*-1.5 * cos(gridP.Y(:)) ./ gridP.X(:).^2];
 % size(A)
 vel = 1;
-Vx_inf = vel*cos(gridVx.y(2:end-1));
-Vy_inf = -vel*sin(gridVy.y(2:end-1));
+Vx_inf = Ux0(end, 2:end-1); vel*cos(gridVx.y(2:end-1));
+Vy_inf = Uy0(end, 2:end-1); -vel*sin(gridVy.y(2:end-1));
 
 [lhs1x, rhs1x] = dirichlet(gridVx, [-1 0]);
 [lhs2x, rhs2x] = dirichlet(gridVx, [+1 0]);
@@ -68,19 +70,24 @@ e = f;
 [Fx, Fy, div] = split(e, gridVx.sz-2, gridVy.sz-2, gridP.sz);
 [Vx1, Vy1, P1] = split(u, gridVx.sz, gridVy.sz, gridP.sz);
 
-disp(norm(Fx(:), inf))
-disp(norm(Fy(:), inf))
-disp(norm(div(:), inf))
+% disp(norm(Fx(:), inf))
+% disp(norm(Fy(:), inf))
+% disp(norm(div(:), inf))
 
 z = zeros(numel(t), 1);
 M = stokes_vanka_redblack(gridP.sz, B);
+h = progress([], 0, 'Iterative Solver');
 for iter = 1:iters
     for k = 1:2
         r = t - B*z;
-        z = z + M{k}*r;
-        norm(r, inf)
+        z = z + M{k}*r;        
     end
+    res = norm(r, inf);
+    progress(h, iter/iters, ...
+        sprintf('Residual = %.3e', res));
 end
+progress(h, []);
+
 [Vx, Vy, P] = split(z, gridVx.sz-2, gridVy.sz-2, gridP.sz);
 P = P - mean(P(:));
 norm(col(P - P1), inf)
--- utils/progress.m
+++ utils/progress.m
@@ -4,6 +4,7 @@ if isempty(handle)
     handle.start = now;
     handle.fig = waitbar(0, '');
     handle.msg = msg;
+    msg = '';
     handle.p = 0;
 elseif isempty(p)
     close(handle.fig);
@@ -12,11 +13,15 @@ elseif p < min(1, handle.p + 1e-2)
     return
 end
 
+if nargin >= 3 && ~isempty(msg)
+    msg = sprintf('\n%s', msg);
+end
+
 handle.p = p;
 
 runtime = datestr(now - handle.start, 'HH:MM:SS');
 runtime = ['Runtime: ' runtime ' [seconds]'];
 
-waitbar(handle.p, handle.fig, runtime);
+waitbar(handle.p, handle.fig, [runtime msg]);
 s = [sprintf('%.0f%%', 100*handle.p) ' - ' handle.msg];
 set(handle.fig, 'Name', s);
