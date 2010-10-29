dd300767dd8572c11f013fd24d601f2dfbaf5737
Roman Zeyde <roman.zeyde@gmail.com>
2010-10-29 16:43:33 +0200

    Stokes solution. Velocity field around sphere with no-slip boundary condition.

---
+++ ng/stokes_velocity.m
@@ -0,0 +1,4 @@
+function [Vr, Vtheta] = stokes_velocity(U, r, theta)
+    Vr = -U .* cos(theta).* (1 - 1.5 ./ r + 0.5 ./ (r.^3));
+    Vtheta = U .* sin(theta).* (1 - 0.75 ./ r - 0.25 ./ (r.^3));
+end
