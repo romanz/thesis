e0c65c3536ced95631d5aacfed61d4af801e95ee
Roman Zeyde <roman.zeyde@gmail.com>
2010-10-29 16:47:52 +0200

    Refactor.

--- ng/stokes_velocity.m
+++ ng/stokes_velocity.m
@@ -1,3 +1,4 @@
+% [Vr, Vtheta] = stokes_velocity(U, r, theta)
 function [Vr, Vtheta] = stokes_velocity(U, r, theta)
     Vr = -U .* cos(theta).* (1 - 1.5 ./ r + 0.5 ./ (r.^3));
     Vtheta = U .* sin(theta).* (1 - 0.75 ./ r - 0.25 ./ (r.^3));
