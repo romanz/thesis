981ac4bf629aca94b7572a7b980995873fcd83a8
Roman Zeyde <roman.zeyde@gmail.com>
2011-01-29 18:12:59 +0200

    Refactor.

--- ng/split.m
+++ ng/split.m
@@ -1,3 +1,6 @@
+% Splits x into seperate variables according to specified sizes.
+%   [x1, x2, x3] = split(x, sz1, sz2, sz3);
+%   where sz{i} = size(x{i}) and x = [x1(:); x2(:); x3(:)];
 function [varargout] = split(x, varargin)
 N = numel(varargin);
 varargout = cell(N);
