d659578d193599f24105a1ec50517b0d7227e8a1
Roman Zeyde <roman.zeyde@gmail.com>
2011-02-22 19:44:48 +0200

    Refactor interpolator.

--- src/interpolator.m
+++ src/interpolator.m
@@ -1,15 +1,15 @@
-function A = interpolator(X, Xi)
-    dir = size(X) - size(Xi);
+function A = interpolator(Z, Zi)
+    dir = size(Z) - size(Zi);
     assert(all(sort(dir) == [0 1]))
-    K1 = false(size(X));
-    K1(1:end-dir(1), 1:end-dir(2)) = true;
-    K2 = shift(K1, dir);
+
+    K = true(size(Z));
+    P1 = expand(shift(K, -dir))';
+    P2 = expand(shift(K, +dir))';
+    d = P2*Z(:) - P1*Z(:); assert(all(d(:) > 0));
     
-    D = X(K2) - X(K1);
-    assert(all(D(:) > 0));
-    A = [(X(K2) - Xi(:)), (Xi(:) - X(K1))] ./ [D D];
+    a1 = (P2 * Z(:) - Zi(:)) ./ d(:);
+    a2 = (Zi(:) - P1 * Z(:)) ./ d(:);
+    A = spdiag(a1) * P1 + spdiag(a2) * P2;
     assert(all(A(:) <= 1));
     assert(all(A(:) >= 0));
-    I = col(1:numel(Xi));
-    A = sparse([I I], [find(K1) find(K2)], A);
 end
