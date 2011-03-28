23352c3806dc711f6f819ef57b7612fec2a964e4
Roman Zeyde <roman.zeyde@gmail.com>
2011-03-28 13:19:56 +0200

    Refactor SECAND method.

--- utils/secant.m
+++ utils/secant.m
@@ -1,21 +1,24 @@
-% SOL = SECANT(F, A, B, ITERS)
+% SOL = SECANT(F, [A B], ITERS)
 % Secant method to f(x)=0 starting at [a,b]. 
 function sol = secant(f, x, iters)
-    x1 = x(1); fprintf('%f -> ', x1); f1 = f(x1); fprintf('%e\n', f1);
-    x2 = x(2); fprintf('%f -> ', x2); f2 = f(x2); fprintf('%e\n', f2);
+    x1 = x(1); f1 = f(x1); status(x1, f1);
+    x2 = x(2); f2 = f(x2); status(x2, f2);
     x3 = next(x1, x2, f1, f2);
     for i = 1:iters
-        if x2 == x3 || abs(x2 - x3) < 1e-8
+        if x2 == x3
             break
         end
-        fprintf('%f -> ', x3);
         f3 = f(x3);
-        fprintf('%e\n', f3);
+        status(x3, f3);
         x1 = x2; f1 = f2;
         x2 = x3; f2 = f3;
         x3 = next(x1, x2, f1, f2);
     end
     sol = x3;
+end
+
+function status(x, f)
+    fprintf('Secant: %f -> %e\n', x, f)
 end
 
 function sol = next(x1, x2, f1, f2)
