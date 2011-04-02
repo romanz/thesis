a60c44628ab1a0136a0452b914cd086e3a79b0e7
Roman Zeyde <roman.zeyde@gmail.com>
2011-04-02 14:08:38 +0300

    Construct graph of u(beta).

--- src/graph.m
+++ src/graph.m
@@ -1,8 +1,8 @@
-betas = linspace(0, 10, 51);
+betas = 0 : 0.5 : 30;
 u = zeros(size(betas));
-
-sol = [];
+sol = steady([], 0, [0 1], 2);
 for i = 1:numel(betas);
-    sol = steady([], (0.1:0.1:1)*betas(i), [0 1], 2);
+    sol = steady(sol, betas(i), sol.Vinf + [0 1], 2);
     u(i) = sol.Vinf;
+    fprintf('\tb = %e\n\tu = %e\n', betas(i), u(i));
 end
