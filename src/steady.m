c61e978b0bf3b1ad4c76e4e93c47740eb1f8d58e
Roman Zeyde <roman.zeyde@gmail.com>
2011-04-03 17:25:47 +0300

    Steady-state continuation docs.

--- src/steady.m
+++ src/steady.m
@@ -1,16 +1,17 @@
-% Steady-state solver
-% Given Beta, find Velocity for zero particle force.
+% Steady-state solver.
+% Given beta, find the velocity for zero particle force.
 function sol = steady(sol, betas, v, iters, varargin)
 
-conf = {'version', 0, 'logging', 0, varargin{:}};
-function f = func(u)
-    sol = main(sol, betas(end), u, conf{:});
-    f = sol.force.total;
-end
+    conf = {'version', 0, 'logging', 0, varargin{:}};
+    function f = func(b, u) % Total force for specified (b, u)
+        sol = main(sol, b, u, conf{:});
+        f = sol.force.total;
+    end
 
-v = [min(v), max(v)];
-sol = main(sol, betas, v(1), conf{:});
-status = @(x, f) fprintf('B = %e, V = %e, F = %e\n', betas(end), x, f);
-secant(@func, v, iters, status);
+    v = [min(v), max(v)];
+    func(betas, v(1)); % Converge near v(1) using continuationin betas.
+    b = betas(end); % Last beta, for steady-state velocity.
+    secant(@(u) func(b, u), v, iters, ...
+        @(u, f) fprintf('B = %e, V = %e, F = %e\n', b, u, f));
 
 end
