ad3965995243b42f2aacad2c88fb269ccbc5ecd8
Roman Zeyde <roman.zeyde@gmail.com>
2011-05-04 16:27:42 +0300

    verbosity setting.

--- src/steady.m
+++ src/steady.m
@@ -4,7 +4,7 @@
 % >> s = steady([], 1, [1 2], 3);
 function [sol, grid] = steady(sol, betas, v, iters, varargin)
 
-    conf = [{'version', 0, 'logging', 1}, varargin];
+    conf = [{'version', 0, 'verbose', 1}, varargin];
     function f = func(b, u) % Total force for specified (b, u)
         [sol, grid] = force(sol, b, u, conf{:});
         f = sol.force.total;
