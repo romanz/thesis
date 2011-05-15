9971510f594955ef0263d7ceb3a38ca97cc1f2aa
Roman Zeyde <roman.zeyde@gmail.com>
2011-05-15 22:46:09 +0300

    Fix streamline's axis .

--- src/test.m
+++ src/test.m
@@ -5,5 +5,8 @@ for a = [0.5]
     sol = steady(sol, sol.beta, sol.Vinf*[1 0.9], 3, 'alpha', a);
     disp(sol)
 end
-streamlines(sol, linspace(0, 1, 50)); 
+%%
+clf;
+streamlines(sol, linspace(0, 1, 50));
+axis equal;
 xlim([-3 3]); ylim([0 3])
