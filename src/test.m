3eb8fba2d3b158f070e978d489cde3769b822935
Roman Zeyde <roman.zeyde@gmail.com>
2011-05-16 22:03:34 +0300

    Comment out frame capture. NOTE: to capture frames, the figure must be active.

--- src/test.m
+++ src/test.m
@@ -1,27 +1,29 @@
 %%
-clc; clear;
-sol = steady([], 1e-3, [1, 0.9], 3, 'alpha', 0);
+clc; clear; tic;
+betas = logspace(-2, 1, 150);
+betas(2)/betas(1) - 1
+sol = steady([], betas(1), [1, 0.9], 3, 'alpha', 0);
 sol.alpha = 0.5;
 x = 1.1;
-M = round(log(10e3)/log(x));
-sols = cell(M, 1);
-for k = 1:M
-    sol.beta = (sol.beta * x);
-    sol = steady(sol, sol.beta, sol.Vinf*[1 0.9], 3);
+sols = cell(numel(betas), 1);
+for k = 1:numel(betas)
+    sol.time = toc;
+    sol = steady(sol, betas(k), sol.Vinf*[1 0.9], 3);
+    sol.time = toc - sol.time;
     sols{k} = sol;
 end
 save
-%%
-load
-clf;
-for k = 1:numel(sols)
-    sol = sols{k};
-    streamlines(sol, linspace(-sol.Vinf, sol.Vinf, 100));
-    axis equal;
-    d = 5;
-    xlim([-d d]);
-    ylim([0 d])
-    F(k) = getframe;    
-end
-%%
-movie(F)
+% %%
+% load
+% clf;
+% for k = 1:numel(sols)
+%     sol = sols{k};
+%     streamlines(sol, linspace(-sol.Vinf, sol.Vinf, 100));
+%     axis equal;
+%     d = 5;
+%     xlim([-d d]);
+%     ylim([0 d])
+%     F(k) = getframe;    
+% end
+% %%
+% movie(F)
