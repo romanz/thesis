c492cd2d59ad64473c59a79a1f424658d7a7c225
Roman Zeyde <roman.zeyde@gmail.com>
2011-06-21 12:58:58 +0300

    Refactor.

--- src/newton_solver.m
+++ src/newton_solver.m
@@ -220,19 +220,3 @@ function S = ident(grid)
     K = find(grid.I(:));
     S = sparse(K, K, 1, grid.numel, grid.numel);
 end
-
-% Splits x into seperate variables according to specified sizes.
-%   [x1, x2, x3] = split(x, sz1, sz2, sz3);
-%   where sz{i} = size(x{i}) and x = [x1(:); x2(:); x3(:)];
-function [varargout] = split(x, varargin)
-    N = numel(varargin);
-    varargout = cell(N);
-    offset = 0;
-    for k = 1:N
-        sz = varargin{k};
-        m = prod(sz);
-        varargout{k} = reshape(x(offset + (1:m)), sz);
-        offset = offset + m;
-    end
-    assert(offset == numel(x), 'Splitting mismatch.');
-end
---
+++ src/split.m
@@ -0,0 +1,15 @@
+% Splits x into seperate variables according to specified sizes.
+%   [x1, x2, x3] = split(x, sz1, sz2, sz3);
+%   where sz{i} = size(x{i}) and x = [x1(:); x2(:); x3(:)];
+function [varargout] = split(x, varargin)
+    N = numel(varargin);
+    varargout = cell(N);
+    offset = 0;
+    for k = 1:N
+        sz = varargin{k};
+        m = prod(sz);
+        varargout{k} = reshape(x(offset + (1:m)), sz);
+        offset = offset + m;
+    end
+    assert(offset == numel(x), 'Splitting mismatch.');
+end
