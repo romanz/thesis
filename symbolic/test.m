88a33110a70d8ddcdaab5fa89e54c2f71d5c8239
Roman Zeyde <roman.zeyde@gmail.com>
2011-10-17 17:18:57 +0200

    O(beta^2) term is O(sin(2t))
    >> pretty(simple(V2))
    
                                        /      1/2     \
                                        | gamma    + 1 |
                          3 sin(2 t) log| ------------ | (W + 9)
                                        |         1/2  |
         9 sin(2 t)                     \  2 gamma     /
      ----------------- - --------------------------------------
               1/2                          8
      16 (gamma    + 1)

--- symbolic/test.m
+++ symbolic/test.m
@@ -1,15 +1,17 @@
 clear;
 syms a r t U W
 clc
-psi = (U/2) * (r^2 - (3*a*r)/2 + a^3/(2*r))*sin(t)^2;
-V = curl(psi);
-L = simple(vector_laplacian(V));
-P = -3*U*a*cos(t)/(2*r^2);
-gP = gradient(P);
-assert(all(L == gP))
-F = radial_stress(V, P, a);
+at = @(f) simple(subs(f, r, 1));
 
-psi = (W/2) * (a*r - a^3/r)*sin(t)^2;
+% psi = -(U/2) * (r^2 - (3*a*r)/2 + a^3/(2*r))*sin(t)^2;
+% V = curl(psi);
+% L = simple(vector_laplacian(V));
+% P = -3*U*a*cos(t)/(2*r^2);
+% gP = gradient(P);
+% assert(all(L == gP))
+% F = radial_stress(V, P, a);
+
+psi = -(W/2) * (r^2 - a^3/r)*sin(t)^2;
 V = curl(psi);
 assert([1 0]*subs(V, r, a) == 0);
 L = simple(vector_laplacian(V));
@@ -20,7 +22,15 @@ Phi1 = ((1/4)*r^-2 - r) * cos(t);
 C1 = (3/4)*r^-2 * cos(t);
 assert(subs(Phi1 + C1, r, 1) == 0)
 assert(subs(diff(Phi1 - C1, r), r, 1) == 0)
+syms gamma phi
+xi = -phi-log(gamma);
+g = 4*log((exp(xi/2)+1)/2);
+V1a = subs(g, phi, 0) * at(diff(Phi1, t));
+U1 = simple(int(V1a * sin(t)^2, 0, pi) / int(sin(t), 0, pi));
+
 V1 = subs(V, a, 1);
+W1 = simple(int(at(V1(2)) * sin(t)^2, 0, pi) / int(sin(t), 0, pi));
+assert(W1 == W)
 
 gC1 = gradient(C1);
 gPhi1 = gradient(Phi1);
@@ -30,16 +40,17 @@ syms A1 A2 A3 A4
 RHS_Phi = simple(-gC1.'*gPhi1);
 Phi2 = 3/32*r^-4*sin(t)^2 - 3/8*r^-1*sin(t)^2 - 3/32*r^-4;
 Phi2 = simple(Phi2);
-Phi2 = Phi2 + A3/r + A4*(3*cos(t)^2-1)/r^3;
 assert(simple(scalar_laplacian(Phi2) - RHS_Phi) == 0)
 
 RHS_C = simple(V1.'*gC1);
-C2 = (-3*W/8*(r^-4*sin(t)^2/2 + 5/4*r^-2*sin(t)^2 - r^-4/2 - r^-2/2)); 
-C2 = C2 + A1/r + A2*(3*cos(t)^2-1)/r^3;
+C2 = 3*W/8*((r^-1 + 1/2*r^-4)*sin(t)^2 - 1/2*r^-4);
 C2 = simple(C2);
-assert(simple(RHS_C - scalar_laplacian(C2)) == 0)
+s = simple(RHS_C - scalar_laplacian(C2));
+assert(s == 0);
 
-at = @(f) simple(subs(f, r, 1));
+C2 = C2 + A1/r + A2*(3*cos(t)^2-1)/r^3;
+Phi2 = Phi2 + A3/r + A4*(3*cos(t)^2-1)/r^3;
+
 Dr = @(f) diff(f, r);
 s1 = at(C2 + Phi2 - Phi1^2/2);
 s2 = at(Dr(C2 - Phi2 - C1*Dr(Phi1)));
@@ -51,27 +62,22 @@ q3 = subs(s2, t, 0);
 q4 = subs(s2, t, pi/2);
 
 S = solve(q1, q2, q3, q4, A1, A2, A3, A4);
-A1 = S.A1;
-A2 = S.A2;
-A3 = S.A3;
-A4 = S.A4;
+C2 = subs(C2, [A1 A2 A3 A4], [S.A1 S.A2 S.A3 S.A4]);
+Phi2 = subs(Phi2, [A1 A2 A3 A4], [S.A1 S.A2 S.A3 S.A4]);
 
-RHS_Phi = simple(-gC1.'*gPhi1);
-Phi2 = 3/32*r^-4*sin(t)^2 - 3/8*r^-1*sin(t)^2 - 3/32*r^-4;
-Phi2 = simple(Phi2);
-Phi2 = Phi2 + A3/r + A4*(3*cos(t)^2-1)/r^3;
 assert(simple(scalar_laplacian(Phi2) - RHS_Phi) == 0)
-
-RHS_C = simple(V1.'*gC1);
-C2 = (-3*W/8*(r^-4*sin(t)^2/2 + 5/4*r^-2*sin(t)^2 - r^-4/2 - r^-2/2)); 
-C2 = C2 + A1/r + A2*(3*cos(t)^2-1)/r^3;
-C2 = simple(C2);
 assert(simple(RHS_C - scalar_laplacian(C2)) == 0)
 
 assert( 0 == at(C2 + Phi2 - Phi1^2/2) );
 assert( 0 == at(Dr(C2 - Phi2 - C1*Dr(Phi1))) );
 
+V2 = subs(g, phi, 0) * at(diff(Phi2, t)) + ...
+    subs(diff(g, phi), phi, 0) * at(Phi1 * diff(Phi1, t));
+V2 = simple(V2);
+
 return
+
+
 P = W*a*cos(t)/r^2;
 gP = gradient(P);
 assert(all(L == gP))
