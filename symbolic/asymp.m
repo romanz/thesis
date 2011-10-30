fe068924e37b63c8c68c5083de49785fd7db3df7
Roman Zeyde <roman.zeyde@gmail.com>
2011-10-30 22:48:55 +0200

    Refactor and debug on gamma=1 shoud yield v=0.

--- symbolic/asymp.m
+++ symbolic/asymp.m
@@ -1,49 +1,55 @@
 function asymp
 
-    syms pi b r t a g U1 U2 real
+    syms pi b r t a g U1 U2 U3 real
     syms U3a U3b real
-    U3 = (2 - U1*a)/16;
-    integral = @(f) int(f, 0, pi);
     
     bnd = @(f) subs(f, r, 1);
     cond = @(phi, c) bnd([phi + log(c); c*Dr(phi) - Dr(c)]);
     slip = @(phi) bnd(dukhin_derjaguin(phi, g));
 
+    % O(beta)
+    % Boundary conditions
     phi1 = (1/4 * r^(-2) - r) * cos(t);
+    c1 = 3/4 * r^(-2) * cos(t);
+    v1 = U1 * curl( (r^-1 - r^2)*sin(t)^2/2 ); 
+    
+    % O(beta^2)
+    % Non-homogeneous solution
     phi2 = (3/32*r^-4  - 3/8*r^-1)*sin(t)^2 - 3/32*r^-4;
+    c2 = a*U1*3/8*((r^-1 + 1/2*r^-4)*sin(t)^2 - 1/2*r^-4);
+    % Boundary conditions
+    phi2 = phi2 + 3*(1/16 - a*U1/32)/r + (a*U1/32 - 1/16)*(3*cos(t)^2 - 1)/r^3;
+    c2 = c2 + 3*(1/16 - a*U1/32)/r + ((5*U1*a)/32 + 1/16)*(3*cos(t)^2 - 1)/r^3;
+    v2 = U2 * curl( (r^-2 - 1)*(cos(t)*sin(t)^2) );
+    p2 = U2 * 2 * r^-3 * (1 - 3*cos(t)^2);
+
+    % O(beta^3)
     phi3 = (15*U1*a + 6)/64*cos(t) + (-3*U1*a/32)*cos(t)^3 ...
          + (15*U1*a + 6)/64*cos(t)^3/r^2 ...
          + ((12 + 15*U1*a)/64*cos(t) - (27 + 18*U1*a)/96*cos(t)^3)/r^3 ...
          + ((2*U1*a - 1)/64*cos(t) + (3 - 6*U1*a)/64*cos(t)^3)/r^5 ...
          + (27 + 9*U1*a)/576*cos(t)^3/r^6;
-    
-    c1 = 3/4 * r^(-2) * cos(t);
-    c2 = a*U1*3/8*((r^-1 + 1/2*r^-4)*sin(t)^2 - 1/2*r^-4);
-    c3 = (3*U1^2*a^2*cos(t)^3)/32 ...
+    c3 = (3*U1^2*a^2*cos(t)^3)/32 - (3*U1*a*cos(t)*(5*U1*a + 2))/64 ...
         - (3*a*cos(t)*(- 8*a*U1^2*cos(t)^2 + 7*a*U1^2 + 2*U1 + 32*U2*cos(t)^2 - 32*U2))/(128*r^3) ...
-        - (3*U1*a*cos(t)*(5*U1*a + 2))/64 ...
         + (a*(cos(t) - 3*cos(t)^3)*(5*a*U1^2 + 2*U1 + 16*U2))/(128*r^5) ...
         + (U1^2*a^2*cos(t)^3)/(32*r^6) - (3*U1*a*cos(t)^3*(5*U1*a + 2))/(64*r^2);
-    
     A1 = 829/28160 + U3b*2/5;
     A2 = -761/5632 - U3b*2;
     A3 = 35/264 - U3a/3 + U3b*5/3;
     A4 = -209/3360 + U3a/3 - U3b*1/15;
-    v1 = U1 * curl( (r^-1 - r^2)*sin(t)^2/2 ); 
-    v2 = U2 * curl( (r^-2 - 1)*(cos(t)*sin(t)^2) );
     v3 = curl( - (sin(t)^2*(1848*r^6*sin(t)^2 - 2079*r^3*sin(t)^2 + 924*r^3 - 7*sin(t)^2 + 10))/(19712*r^5));
     v3 = v3 - curl( A1*(1-5*cos(t)^2)*sin(t)^2/r^3 + A2*sin(t)^4/r + ...
         A3*sin(t)^2/r + A4*r^2*sin(t)^2);
+    
+    % Zero force
+    U3 = (2 - U1*a)/16;
     v3 = v3 - curl(U3*(r^2 - (3/2)*r + (1/2)/r)*sin(t)^2/2);
     
-    p2 = U2 * 2 * r^-3 * (1 - 3*cos(t)^2);
     p3 = (-cos(t)*(- 1584*r^6*cos(t)^2 + 1584*r^6 + 1188*r^3*cos(t)^2 - 396*r^3 + 45*cos(t)^2 + 17))/(1408*r^8);
     p3 = p3 - A2*(6*cos(t) - 10*cos(t)^3)/r^4;
     p3 = p3 + U3*3/2*cos(t)/r^2;
     
-    phi2 = phi2 + 3*(1/16 - a*U1/32)/r + (a*U1/32 - 1/16)*(3*cos(t)^2 - 1)/r^3;
-    c2 = c2 + 3*(1/16 - a*U1/32)/r + ((5*U1*a)/32 + 1/16)*(3*cos(t)^2 - 1)/r^3;
-    
+    % Boundary conditions
     phi3 = phi3 + ...
         ((183*U1^2*a^2)/2560 - (839*U1*a)/2560 - 47/256) * cos(t)/r^2 + ...
         (- (19*U1^2*a^2)/5120 + (97*U1*a)/5120 + (3*U2*a)/320 + 21/1280) * (5*cos(t)^3 - 3*cos(t))/r^4;
@@ -73,6 +79,7 @@ function asymp
     
     f = force(v, p, phi);
     f = series(f, b, 0, 3);
+    
     assert_zero( f, 'Zero force' )
         
     vb = simple(subs(v, r, 1));
@@ -85,7 +92,7 @@ function asymp
     vs = slip(phi);
     vs = series(vs, b, 0, 3);
     slip_error = subs(vs - vb(2), [U1 U2 U3a U3b], [W1 W2 W3a W3b]);
-
+    
     assert_zero( vb(1), 'No penetration' );
     assert_zero( slip_error, 'Slip condition' );
     
