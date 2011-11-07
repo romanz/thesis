c07020587529fe6fab7a5a97559b5c89ca18c58c
Roman Zeyde <roman.zeyde@gmail.com>
2011-11-07 19:43:36 +0200

    Good correspondence for O(beta^3)

--- src/solver.m
+++ src/solver.m
@@ -6,14 +6,14 @@ function solver
     tic;
     clc;
     sol = struct( ...
-        'radius', logspace(0, 8, 300), ...
-        'theta', linspace(0, pi, 200), ...
+        'radius', logspace(0, 7, 400), ...
+        'theta', linspace(0, pi, 50), ...
         'gamma', 1, ...
         'alpha', 0, ...
         'maxres', 1e-11, ...
-        'iters', [2 5] ...    
+        'iters', [2 6] ...    
     );
-    betas = 10.^(-3:0.5:-1);
+    betas = 10.^(-4:1:0);
     sol = force(sol); % initialize force solver
     solutions = cell(numel(betas), 1);
     for k = 1:numel(betas)
@@ -22,12 +22,15 @@ function solver
         alphas = [0]; % XXX
         for a = alphas
             sol.alpha = a;
-            [sol, V, F] = steady(sol, sol.Vinf + [0, 0.001], 5);
+            [sol, V, F] = steady(sol, sol.Vinf + [0, 0.001], 10);
         end
         solutions{k} = sol;
         pause(0);
     end
     save
     tone(300, 0.1)
+    b = []; for k = 1:numel(solutions), b(k) = solutions{k}.beta; end
+    v = []; for k = 1:numel(solutions), v(k) = solutions{k}.Vinf; end
+    plot(log10(b), log10([v; 0.042*b.^3]), '.-')
 end
 
