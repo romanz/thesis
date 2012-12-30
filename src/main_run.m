7ed13458632e770eb4ca9411df16cb2e2e289c86
Roman Zeyde <roman.zeyde@gmail.com>
2012-12-30 16:52:52 +0200

    Add main_run()

--- src/main_run.m
+++ src/main_run.m
@@ -1,7 +1,13 @@
-function main_run
+function main_run(N, Rmax, betas)
 
-%                 main_ephor([], g, betas, struct('alpha', 0.2, 'Du', 0.5, 'zeta', 6));
-%                 main_ephor([], g, betas, struct('alpha', 0.4, 'Du', 0.5, 'zeta', 6));
+%    N = 2.^(8);
+%    Rmax = 100;
+%    betas = 0.1 * 2.^[-2:0.5:5];
+    for n=N 
+        for r=Rmax
+            g = grids(r, n+1, n+1, 1.0);
+            main_ephor([], g, betas, struct('alpha', 0.5, 'Du', 1.0, 'zeta', 10));
+        end
+    end
 
 end
-
